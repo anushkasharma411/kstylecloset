79dd65c745b8ca250fc45352c42dbe7c66b5b2c0
anushka sharma <anushkagpsharma@gmail.com>
2026-04-12 23:30:05 +0530

    final urls fix

--- fashion_project/urls.py
+++ fashion_project/urls.py
@@ -2,14 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from fashion_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
-    path('', include('fashion_app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns = [
-    # your urls
+    path('', include('fashion_app.urls')),   # 👈 ONLY THIS
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
